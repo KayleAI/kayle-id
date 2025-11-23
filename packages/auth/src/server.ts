4c658d88040119752add273bceac86ad7f5a36a5
Arsen Shkrumelyak <arsen@nerverift.com>
2025-11-23 15:53:10 +0000

    set up auth + database

--- packages/auth/src/server.ts
+++ packages/auth/src/server.ts
@@ -1,13 +1,22 @@
 import { env } from "@kayle-id/config/env";
 import { db } from "@kayle-id/database/drizzle";
-import { betterAuth } from "better-auth";
+import { type BetterAuthOptions, betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+const user = {
+  modelName: "auth_users",
+  deleteUser: {
+    // TODO: implement delete user
+  },
+} satisfies BetterAuthOptions["user"];
 
 export const auth = betterAuth({
   secret: env.BETTER_AUTH_SECRET,
   url: env.PUBLIC_BETTER_AUTH_URL,
   database: drizzleAdapter(db, {
     provider: "pg",
+    usePlural: false,
+    camelCase: false,
   }),
   experimental: {
     joins: true,
@@ -22,6 +31,16 @@ export const auth = betterAuth({
   telemetry: {
     debug: false,
     enabled: false,
+  },
+  user,
+  account: {
+    modelName: "auth_accounts",
+  },
+  session: {
+    modelName: "auth_sessions",
+  },
+  verification: {
+    modelName: "auth_verifications",
   },
 });
 
