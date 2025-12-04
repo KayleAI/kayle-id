737bd39811d7c3cb1edccfbc4c3a452f279d9cb9
Arsen Shkrumelyak <arsen@nerverift.com>
2025-12-04 12:59:08 +0000

    disable upstash redis in test environment

--- packages/database/src/redis.ts
+++ packages/database/src/redis.ts
@@ -1,9 +1,13 @@
 import { env } from "@kayle-id/config/env";
 import { Redis } from "@upstash/redis";
 
-const redis = new Redis({
-  url: env.REDIS_URL,
-  token: env.REDIS_TOKEN,
-});
+const redis =
+  process.env.NODE_ENV === "test"
+    ? // TODO: Use a mock Redis for testing
+      (null as unknown as Redis)
+    : new Redis({
+        url: env.REDIS_URL,
+        token: env.REDIS_TOKEN,
+      });
 
 export { redis };
