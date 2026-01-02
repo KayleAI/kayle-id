b8e04210c64408ee5c108844c8beb4e14a21c648
Arsen Shkrumelyak <arsen@nerverift.com>
2026-01-02 15:42:52 +0000

    wip: initialise iOS app

---
+++ apps/ios/Kayle ID/Kayle_IDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kayle_IDApp.swift
+//  Kayle ID
+//
+//  Created by Arsen on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Kayle_IDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
